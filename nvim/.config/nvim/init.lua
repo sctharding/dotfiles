eb71e4fcaf777c1e5558fc24b24bfb2d551b480c
Scott Harding <sctharding@googlemail.com>
2024-09-02 21:39:56 +0100

    adding more

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -973,5 +973,8 @@ require("lazy").setup({
 	},
 })
 
+vim.keymap.set("n", "sp", ":vsplit<CR>")
+vim.keymap.set("n", "sv", ":split<CR>")
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
