de041fa88be76f572a985193e845f74addc60c1b
Scott Harding <sctharding@googlemail.com>
2024-09-11 19:51:09 +0100

    updating colorscheme and tmux bindings

--- nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,57 +1,74 @@
 return {
-	"catppuccin/nvim",
+	"rose-pine/neovim",
+	name = "rose-pine",
 	lazy = false,
-	name = "catppuccin",
+	priority = 1000,
 	config = function()
-		require("catppuccin").setup({
-			flavour = "frappe", -- latte, frappe, macchiato, mocha
-			background = { -- :h background
-				light = "latte",
-				dark = "frappe",
-			},
-			transparent_background = false, -- disables setting the background color.
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = false, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
-			},
-			no_italic = false, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = { "italic" }, -- Change the style of comments
-				conditionals = { "italic" },
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-				-- miscs = {}, -- Uncomment to turn off hard-coded styles
-			},
-			color_overrides = {},
-			custom_highlights = {},
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				nvimtree = true,
-				treesitter = true,
-				notify = false,
-				mini = {
-					enabled = true,
-					indentscope_color = "",
-				},
-				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-			},
+		local rosepine = require("rose-pine")
+		rosepine.setup({
+			variant = "moon",
+			dark_variant = "moon",
+			dim_inactive_windows = false,
+			extend_background_behind_borders = true,
 		})
-
-		-- setup must be called before loading
-		vim.cmd.colorscheme("catppuccin-frappe")
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
+
+-- return {
+-- 	"catppuccin/nvim",
+-- 	lazy = false,
+-- 	name = "catppuccin",
+-- 	config = function()
+-- 		require("catppuccin").setup({
+-- 			flavour = "frappe", -- latte, frappe, macchiato, mocha
+-- 			background = { -- :h background
+-- 				light = "latte",
+-- 				dark = "frappe",
+-- 			},
+-- 			transparent_background = false, -- disables setting the background color.
+-- 			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+-- 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+-- 			dim_inactive = {
+-- 				enabled = false, -- dims the background color of inactive window
+-- 				shade = "dark",
+-- 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+-- 			},
+-- 			no_italic = false, -- Force no italic
+-- 			no_bold = false, -- Force no bold
+-- 			no_underline = false, -- Force no underline
+-- 			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+-- 				comments = { "italic" }, -- Change the style of comments
+-- 				conditionals = { "italic" },
+-- 				loops = {},
+-- 				functions = {},
+-- 				keywords = {},
+-- 				strings = {},
+-- 				variables = {},
+-- 				numbers = {},
+-- 				booleans = {},
+-- 				properties = {},
+-- 				types = {},
+-- 				operators = {},
+-- 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+-- 			},
+-- 			color_overrides = {},
+-- 			custom_highlights = {},
+-- 			integrations = {
+-- 				cmp = true,
+-- 				gitsigns = true,
+-- 				nvimtree = true,
+-- 				treesitter = true,
+-- 				notify = false,
+-- 				mini = {
+-- 					enabled = true,
+-- 					indentscope_color = "",
+-- 				},
+-- 				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+-- 			},
+-- 		})
+--
+-- 		-- setup must be called before loading
+-- 		vim.cmd.colorscheme("catppuccin-frappe")
+-- 	end,
+-- }
